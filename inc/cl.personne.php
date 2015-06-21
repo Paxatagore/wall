d03d5edfa1c04039cf1f72a169a0e439c3173b35
Paxatagore <Matthieu.duclos@gmail.com>
2015-06-21 11:20:37 +0200

    Ajout des latitudes et longitudes de la résidence des personnes.
    Recherche automatique et stockage dans la base.

--- inc/cl.personne.php
+++ inc/cl.personne.php
@@ -9,19 +9,36 @@ class personne extends alpha {
 	var $adresse ;
 	var $ville ;
 	var $pays ;
+	var $latitude ;
+	var $longitude ; 
 	var $newsletter ;
 	var $motdepasse ;
 	var $admin ;
 	var $derniere_connexion ;
 	
 	
-	var $fields		= array("num", "nom", "prenom", "mail", "date_naissance", "lieu_naissance", "adresse", "ville", "pays", "newsletter", "motdepasse", "admin", "derniere_connexion") ;
-	var $jsonliste 	= array("num", "nom", "prenom", "mail", "date_naissance", "lieu_naissance", "adresse", "ville", "pays", "newsletter", "admin", "derniere_connexion") ;
+	var $fields		= array("num", "nom", "prenom", "mail", "date_naissance", "lieu_naissance", "adresse", "ville", "pays", "latitude", "longitude", "newsletter", "motdepasse", "admin", "derniere_connexion") ;
+	var $jsonliste 	= array("num", "nom", "prenom", "mail", "date_naissance", "lieu_naissance", "adresse", "ville", "pays", "latitude", "longitude", "newsletter", "admin", "derniere_connexion") ;
 	var $eJson ;
 	
 	function beforetreat() {
 		//la date de naissance
 		$this->date_naissance = mysqldb::date_to_mysql($this->date_naissance) ;
+		//géoencodage de l'adresse
+		if ($this->ville != "") {
+			$string = str_replace (" ", "+", urlencode($this->ville."+".$this->adresse)) ;
+			$details_url = "http://maps.googleapis.com/maps/api/geocode/json?address=".$string."&sensor=false" ;
+			$ch = curl_init() ;
+			curl_setopt($ch, CURLOPT_URL, $details_url) ;
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1) ;
+			$response = json_decode(curl_exec($ch), true) ;
+			if ($response['status'] == 'OK') {
+				$geometry = $response['results'][0]['geometry'] ;
+				$this->longitude = $geometry['location']['lng'] ;
+				$this->latitude = $geometry['location']['lat'] ;
+			}
+			//$eJson = '{"string":"'.$string.'", "response":"'.$response.'"}' ;
+		}
 		return 1 ;
 	}
 	
