9ddbfe8905a4c2dc8a158a46c4b5eac169991a7b
Paxatagore <Matthieu.duclos@gmail.com>
2015-08-09 14:26:11 +0200

    Nettoyage

--- app/cousinade2015.php
+++ app/cousinade2015.php
@@ -9,8 +9,7 @@ menu() ;
 Retrouvez ici les deux présentations qui ont été diffusées samedi et dimanche :
 
 <h4>Le powerpoint d'Yves</h4>
-<div class="information"><a href="../pdf/Tousaupetitroc.pdf">Téléchargez le document PDF</a>.</div>
-<object id="arbre_genealogique" height="900" data="../pdf/Tousaupetitroc.pdf" type="application/pdf" title="Les photos de famille"></object> 
+<div class="information"><a href="../pdf/Tousaupetitroc.pdf">Téléchargez le document PDF</a> (attention - le document pèse 44 MO !).</div>
 <h4>Le quiz de Guillaume</h4>
 <div class="information">Guillaume a publié son quizz <a href="https://prezi.com/evezmxyy8-dt/le-quizz-pour-brier-en-famille/#">à cette adresse internet</a>.</div>
 <iframe src="https://prezi.com/embed/evezmxyy8-dt/?bgcolor=ffffff&amp;lock_to_path=0&amp;autoplay=0&amp;autohide_ctrls=0&amp;landing_data=bHVZZmNaNDBIWklacTNtbFpjdVRkUlFZdXJPeGFJOVBwK2tGOHVqMWhvVUowQWgveml2WGEweFBYc0JJN05VUXd0SXBJaHBSZE91ME4rbWJvYml6V2xjVVJKNjQ5Q3QyTHdpNy9ORlpuZVowbjYzbDk3YUc1Zz09&amp;landing_sign=ZTAjuFFnv3Wmf5C4lxRiuV_0LUICLQb2EBOitS-91RQ#" allowfullscreen="" mozallowfullscreen="" webkitallowfullscreen="" id="iframe_container" frameborder="0" height="800" width="1150"></iframe>
